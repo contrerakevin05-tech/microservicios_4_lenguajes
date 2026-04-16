1edcbfc23d53cdafb5a45978e5935d40c4433d97
contrerakevin05-tech <contrerakevin05@gmail.com>
2026-04-15 22:05:13 -0500

    Add CORS headers and server start message

--- suma/index.js
+++ suma/index.js
@@ -1,6 +1,19 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
+
+  // 🔥 CORS HEADERS (IMPORTANTE)
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  // manejar preflight (aunque GET casi no lo usa)
+  if (req.method === 'OPTIONS') {
+    res.writeHead(200);
+    res.end();
+    return;
+  }
+
   const url = req.url.split('/');
 
   if (url[1] === 'suma') {
@@ -11,7 +24,12 @@ const server = http.createServer((req, res) => {
 
     res.writeHead(200, { 'Content-Type': 'application/json' });
     res.end(JSON.stringify({ resultado }));
+  } else {
+    res.writeHead(404);
+    res.end();
   }
 });
 
-server.listen(process.env.PORT || 3000);
+server.listen(process.env.PORT || 3000, () => {
+  console.log("Servidor corriendo");
+});
